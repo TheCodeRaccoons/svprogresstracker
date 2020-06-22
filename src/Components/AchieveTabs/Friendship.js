aece4eaf71bbe31a426f12e487c7397e52579b7d
Joorch <jacortez_94@hotmail.com>
2020-06-22 17:57:06 -0500

    Added all the acchievements to the friendship section but the last one

--- src/Components/AchieveTabs/Friendship.js
+++ src/Components/AchieveTabs/Friendship.js
@@ -16,12 +16,44 @@ class Friendship extends React.Component {
         return hearts;        
     }
 
-    componentWillMount() {   
+    GetFriendAcchievements = () => {
+        let totalFive = 0
+        let totalTen = 0
+        this.state.Friendship.forEach(el => { 
+            if(el.level >= 5){
+                totalFive += 1;
+            } 
+            if(el.level >= 10){
+                totalTen += 1;
+            } 
+        }); 
+
+        this.setState({
+            five: totalFive,
+            ten: totalTen
+        }) 
+    }
+
+
+    componentDidMount() {   
+        this.GetFriendAcchievements();
     } 
 
     render() {
         return ( 
             <div className="progress-container-flex">    
+
+                <h2>Friendship Achievements</h2>
+                    <ul className="a-List"> 
+                        <li>A New Friend: {(this.state.five >= 1) ? <span className="completed">You have this achievement</span> : <span className="pending">You need to increase your friendship with {1 - this.state.five} Villagers</span> } </li>
+                        <li>Cliques:  {(this.state.five >= 4) ? <span className="completed">You have this achievement</span> : <span className="pending">You need to increase your friendship with {4 - this.state.five} Villagers </span>}</li>
+                        <li>Networking : {(this.state.five >= 10) ? <span className="completed">You have this achievement</span> : <span className="pending">You need to increase your friendship with {10 - this.state.five} Villagers </span>}</li>
+                        <li>Popular: {(this.state.five >= 20) ? <span className="completed">You have this achievement</span> : <span className="pending">You need to increase your friendship with {20 - this.state.five} Villagers </span>}</li>
+                        <li>Best Friends: {(this.state.ten >= 1) ? <span className="completed">You have this achievement</span> : <span className="pending">You need to increase your friendship with {1 - this.state.ten} Villagers </span>} </li>
+                        <li>The Beloved Farmer:  {(this.state.ten >= 8) ? <span className="completed">You have this achievement</span> : <span className="pending"> You need to increase your friendship with {8 - this.state.ten} Villagers </span> }</li>
+                        <li>Introductions quest: TBD </li>
+                    </ul>
+
                 {this.state.Friendship.map((d, i) => 
                     <div className="npc-data"  key={i}>
                         <h2>{d.name}</h2>
