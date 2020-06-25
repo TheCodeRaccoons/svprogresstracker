3111e2393f8f2fa6da64c36cec6d297279b56590
Joorch <jacortez_94@hotmail.com>
2020-06-25 18:30:44 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Main from './Components/Main/Main'
 import './App.css'
 import './Components/tabs-css/tabs.css'
+import {Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Route exact path={`/`} render={ () => < Main />} />
     </div>
   );
 }
