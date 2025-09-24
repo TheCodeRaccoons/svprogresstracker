ddced77f803e1c7ab612ff3dd6d2f48b033c0da4
Joorch <jacortez_94@hotmail.com>
2025-09-23 21:26:29 -0700

    Refactor Achievements to functional component
    Converted Achievements from a class-based React component to a functional component using hooks and props. This simplifies the code and improves readability while maintaining existing functionality.

--- src/Components/Achievements/Achievements.tsx
+++ src/Components/Achievements/Achievements.tsx
@@ -21,96 +21,89 @@ import Monsters from '../AchieveTabs/Monsters';
 import Shipping from '../AchieveTabs/Shipping'
 import Earnings from '../AchieveTabs/moneyEarned'
 import Collection from '../AchieveTabs/Collection'
-import Quests from '../AchieveTabs/quests' 
+import Quests from '../AchieveTabs/quests'
 
-class Achievements extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            achievementImg: [
-                {img:AchCook, alt: "Cooking"}, 
-                {img:AchCraft, alt: "Crafting"}, 
-                {img:AchCrops, alt: "Crops"}, 
-                {img:AchFishing, alt: "Fishing"}, 
-                {img:AchFriendship, alt: "Friendship"}, 
-                {img:AchGuild, alt: "Guild"}, 
-                {img:AchItems, alt: "Items"}, 
-                {img:AchMoney, alt: "Money"}, 
-                {img:AchMuseum, alt: "Museum"}, 
-                {img:AchQuests, alt: "Quests"}, 
-                {img:AchGrandpa, alt: "Grandpa's evaluation"}]
-        }
-    }
+const Achievements = (props) => {
+    const achievementImg = [
+        {img: AchCook, alt: "Cooking"}, 
+        {img: AchCraft, alt: "Crafting"}, 
+        {img: AchCrops, alt: "Crops"}, 
+        {img: AchFishing, alt: "Fishing"}, 
+        {img: AchFriendship, alt: "Friendship"}, 
+        {img: AchGuild, alt: "Guild"}, 
+        {img: AchItems, alt: "Items"}, 
+        {img: AchMoney, alt: "Money"}, 
+        {img: AchMuseum, alt: "Museum"}, 
+        {img: AchQuests, alt: "Quests"}, 
+        {img: AchGrandpa, alt: "Grandpa's evaluation"}
+    ];
 
-    render() {
-        return ( 
-            <div className="file-container">
-            <img className="star" alt="star" src={Star}></img>
-                <Tabs>
-                    <TabList className="achievement">                       
-                        {this.state.achievementImg.map( (img, i) => <Tab key={i}> <img src={img.img} alt={img.alt} title={img.alt} className="tab-ico"></img></Tab> )}
-                    </TabList> 
-                        
-                <TabPanel> 
-                    <section className="achievement-container"> 
-                        <Food recipesCooked={this.props.recipesCooked}></Food>
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container">  
-                        <Crafting itemsCrafted={this.props.itemsCrafted}></Crafting>
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container">
-                        <Crops cropsShipped = {this.props.cropsShipped} />
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container">
-                        <Fish fishCaught = {this.props.fishCaught} />
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container">
-                        <Friendship friendship={this.props.friendship}/>
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container">
-                        <Monsters monstersKilled={this.props.monstersKilled}/>
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container">
-                        <Shipping shippedItems={this.props.shippedItems}/>
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container">
-                        <Earnings moneyEarned={this.props.moneyEarned}/>
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container">
-                        <Collection museumCollection={this.props.museumCollection} />
-                    </section>
-                </TabPanel> 
-                <TabPanel> 
-                    <section className="achievement-container"> 
-                        <Quests questsDone={this.props.questsDone} specialReq={this.props.specialReq} pendingSpecialReq={this.props.pendingSpecialReq} />
-                    </section>
-                </TabPanel>
-                <TabPanel> 
-                    <section className="achievement-container"> 
-                        <Quests questsDone={this.props.questsDone} specialReq={this.props.specialReq} pendingSpecialReq={this.props.pendingSpecialReq} />
-                    </section>
-                </TabPanel>
-                </Tabs>
-            </div>
-        );
-    }
-}
+    return ( 
+        <div className="file-container">
+        <img className="star" alt="star" src={Star}></img>
+            <Tabs>
+                <TabList className="achievement">                       
+                    {achievementImg.map((img, i) => <Tab key={i}> <img src={img.img} alt={img.alt} title={img.alt} className="tab-ico"></img></Tab>)}
+                </TabList> 
+                    
+            <TabPanel> 
+                <section className="achievement-container"> 
+                    <Food recipesCooked={props.recipesCooked}></Food>
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container">  
+                    <Crafting itemsCrafted={props.itemsCrafted}></Crafting>
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container">
+                    <Crops cropsShipped={props.cropsShipped} />
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container">
+                    <Fish fishCaught={props.fishCaught} />
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container">
+                    <Friendship friendship={props.friendship}/>
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container">
+                    <Monsters monstersKilled={props.monstersKilled}/>
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container">
+                    <Shipping shippedItems={props.shippedItems}/>
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container">
+                    <Earnings moneyEarned={props.moneyEarned}/>
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container">
+                    <Collection museumCollection={props.museumCollection} />
+                </section>
+            </TabPanel> 
+            <TabPanel> 
+                <section className="achievement-container"> 
+                    <Quests questsDone={props.questsDone} specialReq={props.specialReq} pendingSpecialReq={props.pendingSpecialReq} />
+                </section>
+            </TabPanel>
+            <TabPanel> 
+                <section className="achievement-container"> 
+                    <Quests questsDone={props.questsDone} specialReq={props.specialReq} pendingSpecialReq={props.pendingSpecialReq} />
+                </section>
+            </TabPanel>
+            </Tabs>
+        </div>
+    );
+};
 
 export default Achievements;
- 
