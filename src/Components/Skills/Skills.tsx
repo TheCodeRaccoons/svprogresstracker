03c2e97709574beeb869733f12ae6aa378c82a7e
Joorch <jacortez_94@hotmail.com>
2025-10-05 22:16:16 -0700

    Refactor Skills components to use functional style
    Converted Skills and SkillContainer components from class-based to functional components using hooks. Centralized skill image imports in a new index.tsx file for better maintainability and updated image references accordingly.

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,28 +1,20 @@
-import React from 'react';   
+import { useState } from 'react'
 import SkillContainer from './SkillContainer/SkillContainer'
-import Fa_img from '../../Media/Skills/Farming.png'
-import Mi_img from '../../Media/Skills/Mining.png'
-import Fo_img from '../../Media/Skills/Foraging.png'
-import Fi_img from '../../Media/Skills/Fishing.png'
-import Co_img from '../../Media/Skills/Combat.png' 
-class Skills extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { 
-            skillImg: [Fa_img, Fi_img, Fo_img, Mi_img, Co_img], 
-            skills: this.props.xp
-        }
-    }
+import {Farming, Mining, Foraging, Fishing, Combat} from '@media/Skills'
 
-    render() {
+const Skills = (props) => {
+    console.log(props)
+    let [skills, setSkills] = useState(props.xp)
+    let [skillImg, setSkillImg] = useState([Farming, Fishing, Foraging, Mining, Combat])
+
         return (
             <section className="stats-container"> 
                 <div className="skill-info"> 
-                    {this.state.skills.map((item, i) => (i === 5) ? "" :  
+                    {skills.map((item, i: number) => (i === 5) ? "" :  
                     <SkillContainer 
                         key={i} 
                         index={i} 
-                        skillImg={this.state.skillImg[i]} 
+                        skillImg={skillImg[i]} 
                         skillName={item.skill} 
                         xp={item.xp} 
                         levelInfo={item.levelInfo}
@@ -30,7 +22,6 @@ class Skills extends React.Component {
                 </div>
             </section>
         );
-    }
 }
 
 export default Skills;
---
+++ src/Media/Skills/index.tsx
@@ -0,0 +1,6 @@
+// Export all tab images
+export { default as Combat } from '@media/skills/Combat.png'
+export { default as Farming } from '@media/skills/Farming.png'
+export { default as Fishing } from '@media/skills/Fishing.png'
+export { default as Foraging } from '@media/skills/Foraging.png'
+export { default as Mining } from '@media/skills/Mining.png'
