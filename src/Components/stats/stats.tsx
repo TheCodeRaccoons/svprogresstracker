ac962a5f7b6c08326268f1ad9ec60c69c1fb7df5
Joorch <jacortez_94@hotmail.com>
2025-10-18 09:43:06 -0700

    Refactor Stats component to use typed props
    Replaces the generic props object with a typed statsPageProps interface and destructures props in the Stats component. Updates all references from props to the new destructured variables for improved type safety and readability.

--- src/Components/stats/stats.tsx
+++ src/Components/stats/stats.tsx
@@ -12,18 +12,24 @@ import Junimo3  from '../../Media/Junimo3B.png'
 import Junimo4  from '../../Media/Junimo4B.png' 
 import Rope     from '../../Media/Rope.png'
 
-const Stats = (props) => {
-    const [farmName] = useState(props.farmName);
+type statsPageProps = {
+    playerData: any;
+    globalFarmName: string;
+    farmhands: any[];
+}
+
+const Stats = ({playerData, globalFarmName, farmhands}: statsPageProps) => {
+    const [farmName] = useState(globalFarmName || "My Farm");
     const [junimos] = useState([Junimo1, Junimo2, Junimo3, Junimo4]);
     const [Activejunimos] = useState([Junimo1a, Junimo2a, Junimo3a, Junimo4a]);
 
-    const titles = props.farmhands.map((item, i) => 
+    const titles = farmhands.map((item, i) => 
         <Tab key={i}>
             <img src={junimos[i + 1]} alt="Jun"></img> {item.playerName}
         </Tab>
     );
 
-    const playerStats = props.farmhands.map((item, i) => (
+    const playerStats = farmhands.map((item, i) => (
         <TabPanel key={i}>
             <section className="wrapper">
                 <Skills xp={item.experience}></Skills>
@@ -51,34 +57,34 @@ const Stats = (props) => {
 
     return ( 
         <div className="file-container">  
-            <div className="farmName"><h2>Farm: {props.playerData.farmName}</h2></div> 
+            <div className="farmName"><h2>Farm: {farmName}</h2></div> 
             <section className="scroller"> 
                 <Tabs>
                     <TabList> 
-                        <Tab><img src={junimos[0]} alt="Junimo" width="25px"></img>{props.playerData.playerName}</Tab>
+                        <Tab><img src={junimos[0]} alt="Junimo" width="25px"></img>{playerData.playerName}</Tab>
                         {titles}
                     </TabList>
                 
                     <TabPanel>
                         <section className="wrapper">
-                            <Skills xp={props.playerData.experience}></Skills>
+                            <Skills xp={playerData.experience}></Skills>
                             <div className="ropes">
                                 <img src={Rope} alt=""></img>
                                 <img src={Rope} alt=""></img>
                             </div> 
                             <Achievements 
-                                recipesCooked={props.playerData.cookedItems} 
-                                itemsCrafted={props.playerData.itemsCrafted}  
-                                cropsShipped={props.playerData.cropsShipped}  
-                                fishCaught={props.playerData.fishCaught}
-                                friendship={props.playerData.friendship}
-                                monstersKilled={props.playerData.monstersKilled}
-                                shippedItems={props.playerData.shippedItems}
-                                moneyEarned={props.playerData.moneyEarned}
-                                museumCollection={props.playerData.museumCollection}
-                                questsDone={props.playerData.questsDone}
-                                specialReq={props.playerData.specialRequests}
-                                pendingSpecialReq={props.playerData.pendingSpecialRequests}
+                                recipesCooked={playerData.cookedItems} 
+                                itemsCrafted={playerData.itemsCrafted}  
+                                cropsShipped={playerData.cropsShipped}  
+                                fishCaught={playerData.fishCaught}
+                                friendship={playerData.friendship}
+                                monstersKilled={playerData.monstersKilled}
+                                shippedItems={playerData.shippedItems}
+                                moneyEarned={playerData.moneyEarned}
+                                museumCollection={playerData.museumCollection}
+                                questsDone={playerData.questsDone}
+                                specialReq={playerData.specialRequests}
+                                pendingSpecialReq={playerData.pendingSpecialRequests}
                             ></Achievements>
                         </section>
                     </TabPanel>
