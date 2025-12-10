a203ccf964c5b94eeb3a36eb6b2da6b21a92af2d
Joorch <jacortez_94@hotmail.com>
2025-12-09 22:41:57 -0800

    Refactor Cooking tab to use common components
    Replaced inline achievement and dish item rendering with reusable AchievementItem and ItemWithCounter components. Updated achievement thresholds and recipe counts to reflect new totals. This improves code maintainability and consistency across the UI.

--- src/Components/AchieveTabs/Cooking/index.tsx
+++ src/Components/AchieveTabs/Cooking/index.tsx
@@ -1,77 +1,63 @@
 import type { cookingDataType } from 'types/displayDataTypes';
 import { Cook, SousChef, GourmetChef } from '@media/Achievements';
 import './Cooking.css';
+import { AchievementItem,ItemWithCounter } from '@components/common';
 
 const Food = ( {cookedItems, knownRecipes, alreadyCookedRecipes} : cookingDataType) => {
     return ( 
         <div className="progress-container">   
             <span className="a-title">
                 <h2>
-                    Has cooked {alreadyCookedRecipes}, knowing {knownRecipes} out of 74 recipes
+                    Has cooked {alreadyCookedRecipes}, knowing {knownRecipes} out of 81 recipes
                 </h2>
             </span>
             <br />
             <h2>Cooking Achievements</h2>
             <div className="section-achievements">
-                <div className="achievement-item">
-                    <img 
-                        className={`${(alreadyCookedRecipes >= 10) ? 
-                        "achieved" : "pending"}`} 
-                        src={Cook}
-                        alt="Cook Achievement"
-                    />
-                    <div>
-                        <h3>Cook:</h3> 
-                        <p>Cook a total of 10 dishes</p>
-                    </div>
-                </div>
-                <div className="achievement-item">
-                    <img 
-                        className={`${(alreadyCookedRecipes >= 25) ? 
-                        "achieved" : "pending"}`} 
-                        src={SousChef}
-                        alt="Sous Chef Achievement"
-                    />
-                    <div>
-                        <h3>Sous Chef:</h3> 
-                        <p>Cook a total of 25 dishes</p>
-                    </div>
-                </div>
-                <div className="achievement-item">
-                    <img 
-                        className={`${(alreadyCookedRecipes >= 74) ? 
-                        "achieved" : "pending"}`} 
-                        src={GourmetChef}
-                        alt="Gourmet Chef Achievement"
-                    />
-                    <div>
-                        <h3>Gourmet Chef:</h3> 
-                        <p>Cook a total of 74 dishes</p>
-                    </div>
-                </div>
+                <AchievementItem 
+                    done={alreadyCookedRecipes >= 10}
+                    image={Cook}
+                    achievementName="Cook"
+                    achievementDesc="Cook a total of 10 dishes"
+                    achievementHoverDesc={alreadyCookedRecipes >= 10 ? 
+                        'Achievement unlocked!' : 
+                        'You need to cook at least 10 dishes to unlock this achievement.'}
+                />
+                <AchievementItem 
+                    done={alreadyCookedRecipes >= 25}
+                    image={SousChef}
+                    achievementName="Sous Chef"
+                    achievementDesc="Cook a total of 25 dishes"
+                    achievementHoverDesc={alreadyCookedRecipes >= 25 ?  
+                            'Achievement unlocked!' : 
+                            `You need to cook ${25 - alreadyCookedRecipes} more dishes to unlock this achievement.`}
+                />
+                <AchievementItem 
+                    done={alreadyCookedRecipes >= 81}
+                    image={GourmetChef}
+                    achievementName="Gourmet Chef"
+                    achievementDesc="Cook a total of 81 dishes"
+                    achievementHoverDesc={alreadyCookedRecipes >= 81 ?  
+                            'Achievement unlocked!' : 
+                            `You need to cook ${81 - alreadyCookedRecipes} more dishes to unlock this achievement.`}
+                />
             </div>
             <br />
             <div className="dishes-grid">
             { cookedItems ? 
-                cookedItems.map((d, i) => 
-                    <a 
-                    href={`https://stardewvalleywiki.com/${d.link}`} 
-                    target="blank" 
-                    key={i}
-                    className="dish-item">
-                        <img 
-                            src={`https://stardew-tracker.s3.amazonaws.com/Cooking/${d.image}.png`} 
-                            alt={d.name} 
-                            className={ (d.times !== undefined) ? ((d.times > 0) ? "done" : "known" ): "" } 
-                            title={(d.times !== undefined) ? 
-                                (d.times > 0) ? `Cooked ${d.name}  ${d.times} times` 
-                                : `You haven't cooked ${d.name}` 
-                                : `You don't know how to cook ${d.name}`
-                            } >
-                        </img>
-                        <p className="times-cooked">x{d.times}</p>
-                    </a>) 
-                : <div>No cooking data available.</div>
+                cookedItems.map((d, i) =>
+                    <ItemWithCounter
+                        link={`https://stardewvalleywiki.com/${d.link}`}
+                        src={`https://stardew-tracker.s3.amazonaws.com/Cooking/${d.image}.png`}
+                        name={d.name}
+                        state={ (d.knownDish) ? ((d.times && d.times > 0) ? "done" : "known" ): "unknown" }
+                        hoverDesc={(d.knownDish) ? 
+                                    (d.times && d.times > 0) ? `Cooked ${d.name}  ${d.times} times` 
+                                    : `You haven't cooked ${d.name}` 
+                                    : `You don't know how to cook ${d.name}`}
+                        times={d.knownDish ? d.times : undefined}
+                    />)
+                    : <div>No cooking data available.</div>
             }
             </div>
         </div>
