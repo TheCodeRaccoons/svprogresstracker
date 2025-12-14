6a34047f754e4a9ec7abe459f4b9d44ec95ea791
Joorch <jacortez_94@hotmail.com>
2025-12-13 17:30:27 -0800

    Refactor Junimo media imports and update component paths
    Moved Junimo image files into a dedicated Junimos directory and created an index.ts for easier imports. Updated Achievements and Skills component paths to use index.ts files. Refactored stats.tsx to use new Junimo imports and updated import paths to use aliases for better maintainability.

---
+++ src/Media/Junimos/index.ts
@@ -0,0 +1,9 @@
+
+export { default as Junimo1 } from './Junimo1.png'
+export { default as Junimo2 } from './Junimo2.png'
+export { default as Junimo3 } from './Junimo3.png'
+export { default as Junimo4 } from './Junimo4.png'
+export { default as Junimo5 }  from './Junimo1B.png'
+export { default as Junimo6 }  from './Junimo2B.png'
+export { default as Junimo7 }  from './Junimo3B.png' 
+export { default as Junimo8 }  from './Junimo4B.png' 
