f50dfcd22f18ef0856c9e61bd098153d7fff3e00
Joorch <jacortez_94@hotmail.com>
2025-12-26 16:15:49 -0800

    Add shipped items parser and update container padding
    Introduced parseShippedItems.ts to process shipped items data and calculate related achievements. Also added bottom padding to the .sv-container section in App.css for improved layout.

---
+++ src/Utility/Parsers/parseShippedItems.ts
@@ -0,0 +1,76 @@
+import { ShipItems } from "@utility/JSON"
+import type { achievementType, generalFormatedItemType, shippedItemsType } from "types/displayDataTypes"
+import type { itemType } from "types/savefile"
+import { GetImages } from "./Utils.ts"
+import { FullShipment } from "@media/Achievements";
+
+let CraftingAchievements = [{
+        goal: ShipItems.shipping.length,
+        image: FullShipment,
+        name: 'Full Shipment',
+        done: false,
+        description: 'Ship all items at least once.',
+        hoverDesc: ''
+    }
+];
+
+export const GetShippedItems = (allShipped: itemType) : shippedItemsType => { 
+    console.log('Parsing shipped items data...');
+    console.log(allShipped);
+    let shippedItems: generalFormatedItemType[] = []
+    let totalShipped = 0;
+    if(!allShipped?.item || allShipped?.item.length === 0) {
+        return {
+            shippedItems,
+            totalShipped,
+            totalItems: ShipItems.shipping.length,
+            achievements: GetAchievementData(0),
+        };
+    }
+    //Parses the shipped info
+    let shipped =  allShipped.item.map( (val) => 
+        {
+            return {
+                id: (val.key.int) ? val.key.int : val.key.string , times: val.value.int
+            }})
+    if(!shipped || shipped.length === 0) {
+        return {
+            shippedItems,
+            totalShipped,
+            totalItems: ShipItems.shipping.length,
+            achievements: GetAchievementData(0),
+        };
+    }
+
+    ShipItems.shipping.forEach(item => {
+        let isShipped = shipped.find(i => i.id === item.item_id );
+        if(isShipped && isShipped.times > 0) totalShipped++;
+        let d = {
+            name: item.item_name,
+            image: GetImages(item.item_name),
+            id: item.item_id,
+            shipped: (shipped && shipped.length > 0 ? shipped.find(i => i.id === item.item_id )?.times || 0 : 0)
+        }
+        shippedItems.push(d);
+    }) 
+    
+    return {
+        shippedItems,
+        totalShipped,
+        totalItems: ShipItems.shipping.length,
+        achievements: GetAchievementData(totalShipped),
+    };
+}
+
+const GetAchievementData = (alreadyShipped: number) : achievementType[] =>  {
+    let achievements = CraftingAchievements.map(ach => {
+        return {
+            ...ach,
+            done: alreadyShipped >= ach.goal,
+            hoverDesc: alreadyShipped >= ach.goal ? 
+                'Achievement unlocked!' : 
+                `You need to ship ${ach.goal - alreadyShipped} more unique items to unlock this achievement.`
+        }
+    });
+    return achievements;
+}
